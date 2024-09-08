9d475941190f541bd4a82c93709afe0d470c5a7b
Александр <zpeoplle@yandex.ru>
2024-09-08 20:55:32 +0300

    Fix MainServiceAppTest

--- main-service/src/test/java/ru/practicum/MainServiceAppTest.java
+++ main-service/src/test/java/ru/practicum/MainServiceAppTest.java
@@ -4,35 +4,19 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
+
 public class MainServiceAppTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public MainServiceAppTest(String testName )
-    {
-        super( testName );
+        extends TestCase {
+
+    public MainServiceAppTest(String testName) {
+        super(testName);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( MainServiceAppTest.class );
+    public static Test suite() {
+        return new TestSuite(MainServiceAppTest.class);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
     }
 }
